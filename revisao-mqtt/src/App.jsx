7910cdeaf74f200f9e63ed30ea9369b53b5ca5ed
Luiz G. <luiz.mwlo76@gmail.com>
2025-08-20 11:40:46 -0300

    construção da revisão + dashboard de leitura de temperatura e umidade

--- revisao-mqtt/src/App.jsx
+++ revisao-mqtt/src/App.jsx
@@ -1,21 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMQTT } from "./hooks/useMQTT";
+import Header from "./components/Header";
+import SensorCard from "./components/SensorCard";
+import StatsCard from "./components/StatsCard";
+import ConnectionInfo from "./components/ConnectionInfo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Configurações do MQTT
+  const brokerHost = "broker.hivemq.com";
+  const brokerPort = 8884;
+  const topic = "LLuizG/lerSensor";
+
+  // Hook customizado para gerenciar MQTT
+  const { temperature, humidity, isConnected, lastUpdate, connectionAttempts } =
+    useMQTT(brokerHost, brokerPort, topic);
 
   return (
-    <div className='container-fluid py-4'>
-      <div className='row justify-content-center'>
-        <div className='col-12 col-xl-10'>
-          
+    <div className="container-fluid py-4">
+      <div className="row justify-content-center">
+        <div className="col-12 col-xl-10">
+          {/* Header com controle de tema */}
+          <Header isConnected={isConnected} lastUpdate={lastUpdate} />
+
+          {/* Cards de Sensores */}
+          <div className="row g-4 mb-4">
+            <SensorCard
+              title="Temperatura"
+              value={temperature}
+              unit="°C"
+              icon="fas fa-thermometer-half"
+              colorClass="temperature-icon"
+              isConnected={isConnected}
+            />
+
+            <SensorCard
+              title="Umidade"
+              value={humidity}
+              unit="%"
+              icon="fas fa-tint"
+              colorClass="humidity-icon"
+              isConnected={isConnected}
+            />
+          </div>
+
+          {/* Estatísticas */}
+          <StatsCard temperature={temperature} humidity={humidity} />
+
+          {/* Informações de Conexão */}
+          <ConnectionInfo
+            brokerHost={brokerHost}
+            topic={topic}
+            isConnected={isConnected}
+            connectionAttempts={connectionAttempts}
+          />
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- revisao-mqtt/src/main.jsx
+++ revisao-mqtt/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 import App from './App.jsx'
+import "./App.css"
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
